c890af1a4609877bc4ba7aff1a699168d56bafc6
Scalavision <scalavision@gmail.com>
2020-03-17 20:42:37 +0100

    removing old files, remove last push that was in wrong branch

--- bash4s/src/old/fileOps.scala
+++ bash4s/src/old/fileOps.scala
@@ -1,16 +1,19 @@
 package old.bash
 
-import domain._
+//import domain._
 
 case class RegexFileSearchApi() {
 
+  /*
   def txt = 
     RegexFileSearch("*.txt")
   
   def bam = 
     RegexFileSearch("*.bam")
+    */
 }
 
+/*
 case class ConditionalExpressions() {
   def a(op: CommandOp) = ConditionalExpression("a", op)
-}
+}*/
