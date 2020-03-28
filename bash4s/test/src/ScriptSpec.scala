6c3b9c88efa354cba6d8a0d666224a0631f0b961
scalavision <scalavison@gmail.com>
2020-03-28 21:23:38 +0100

    adding first test that works

--- bash4s/test/src/ScriptSpec.scala
+++ bash4s/test/src/ScriptSpec.scala
@@ -7,22 +7,21 @@ import zio.test.Assertion._
 import bash4s._
 import scripts._
 
+object TargetScript{
+  def apply(scriptName: String): String =
+    os.read(
+      os.pwd / "bash4s" / "test" / "resources" / s"$scriptName"
+    ).linesIterator.toList.mkString("\n") + "\n"
+}
+
 object ScriptSpec extends DefaultRunnableSpec {
 
   val suite1 = suite("Bash Dsl in Scala")(
 
     test("workDir example") {
-
       val testDir = dirPath"/tmp/workspace/job1"
-
       val workDir = WorkDir(testDir) 
-
-      workDir.script.printRich()
-      workDir.script.save(os.Path("/tmp/wd"))
-      pprint.pprintln(workDir.name)
-//      val result = workDir.run()
- //     pprint.pprintln(result)
-      assert(1)(equalTo(1))
+      assert(workDir.script.txt)(equalTo(TargetScript("WorkDir")))
     },
 
     test("test if else for bash script") {
