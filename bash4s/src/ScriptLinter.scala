0e851f19976604ac8abe79755eda1ef847957ab6
scalavision <scalavison@gmail.com>
2020-03-28 19:15:23 +0100

    basic reproducible indentation is working

--- bash4s/src/ScriptLinter.scala
+++ bash4s/src/ScriptLinter.scala
@@ -4,10 +4,14 @@ import domain._
 
 sealed trait ParserContext
 
-final case object LineContext extends ParserContext
-final case object IfContext extends ParserContext
-final case object ElIfContext extends ParserContext
-final case object ElseContext extends ParserContext
+final case object LineCtx extends ParserContext
+final case object IfCtx extends ParserContext
+final case object ElIfCtx extends ParserContext
+final case object ElseCtx extends ParserContext
+
+// i.e. use a stack instead of List for the ParserContext
+final case class LintData(text: String, ctx: List[ParserContext], indent: Int)
+final case class Indenter(txt: String, indent: Int)
 
 object ScriptLinter {
 
@@ -55,9 +59,9 @@ object ScriptLinter {
       newLineLoop(s.split("\n"), LineContext, "", "")
     }*/
 
-    def startWord: String => String = s => {
+    def startWord: String => (String, String) = s => {
      val x = s.split(" ").filter(_.nonEmpty)
-     x.head
+     (x.head, x.tail.mkString(" "))
     }
 
     def endWord: String => String = s => {
@@ -70,33 +74,42 @@ object ScriptLinter {
       val cleanedForMultiNewLines: String = cleanedForMultiSpace.replaceAll("\n+", "\n")
       /*
       cleanedForMultiNewLines.split("\n").toList.map { l =>
-        
-        val w: String = startWord(l)
+
+        val w: (String,String) = startWord(l)
 
         w match {
-          case "else" => "else"
-          case "fi" => "fi"
-          case _ => l
+          case ("else", w) => "else" + w
+          case ("fi", w) => "fi" + w
+          case _ => l.dropWhile(_ == ' ')
         }
 
-        w + " " + l.split(" ").drop(1).tail.mkString(" ")
-
       }.mkString("\n")
+      
       */
       // use folding to keep track of state 
-      cleanedForMultiNewLines.split("\n").toList.map { l =>
+      cleanedForMultiNewLines.split("\n").toList.foldLeft(Indenter("", 0)) { (acc, l) =>
 
-        val w: String = startWord(l)
+        val w: (String,String) = startWord(l)
 
         w match {
-          case "else" => "else"
-          case "fi" => "fi"
-          case _ => l
+          case("if", w) => acc.copy(
+            txt = acc.txt + "if" + w + "\n",
+            indent = acc.indent + 2
+          )
+          case ("else", w) => acc.copy(
+            txt = acc.txt + "else" + w + "\n",
+          )
+          case ("fi", w) => acc.copy(
+            txt = acc.txt + "fi" + w + "\n",
+            indent = acc.indent - 2
+          )
+          case _ => 
+            acc.copy(
+              txt = acc.txt + List.fill(acc.indent)(' ').mkString + l.dropWhile(_ == ' ') + "\n"
+            )
         }
 
-        w + " " + l.split(" ").drop(1).tail.mkString(" ")
-
-      }.mkString("\n")
+      }.txt
     }
 
     def simplify(op: CommandOp) = {
