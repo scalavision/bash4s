fa484f350651dd0ef616911ddac0be3e3791d7d0
Scalavision <scalavision@gmail.com>
2020-04-05 17:40:54 +0200

    snapshot before pull

--- biology/test/src/bio/BioSpec.scala
+++ biology/test/src/bio/BioSpec.scala
@@ -8,54 +8,38 @@ import zio.test.Assertion._
 import bio.dsl._
 import bash4s.dsl._
 
-
-object TestCases {
-
-  val path = file"/test/hello".fasta
-  val bwaIndex = Bwa.BuildIndex(path)
-
-  val r1 = file"/path/to/fastq".fastq.gz
-  val r2 = file"/path/to/fastq".fastq.gz
-  val ref = file"/path/to/ref".fasta
-  val cores = 12.cores
-  val readGroupInfo = txt"@RG:Test"
-
-  val mapAndAlign = Bwa.MapAndAlign(
-    r1,
-    r2,
-    ref,
-    cores,
-    readGroupInfo
-  )
-
-}
-
 object BioSpec extends DefaultRunnableSpec {
-  import TestCases._
 
   val suite1 = suite("Bwa methods")(
     test("index fasta file") {
-      pprint.pprintln(bwaIndex.script)
+
+      val path = file"/test/hello".fasta
+      println(path)
+
+      pprint.pprintln(Bwa.BuildIndex(path).script)
+
       assert(1)(equalTo(1))
     },
-
     test("map and align fastq files") {
+
+      val r1 = file"/path/to/fastq".fastq.gz
+      val r2 = file"/path/to/fastq".fastq.gz
+      val ref = file"/path/to/ref".fasta
+      val cores = 12.cores
+      val readGroupInfo = txt"@RG:Test"
+
+      val mapAndAlign = Bwa.MapAndAlign(
+        r1,
+        r2,
+        ref,
+        cores,
+        readGroupInfo
+      )
 
       pprint.pprintln(mapAndAlign.script)
 
       assert(1)(equalTo(1))
-    },
-    
-    test("combinations of scripts") {
-      val all = 
-        bwaIndex.op && 
-          mapAndAlign.op
-
-      pprint.pprintln(all.txt)
-      
-      assert(1)(equalTo(1))
     }
-    
   )
 
   def spec = suite("BioSpec")(suite1)
